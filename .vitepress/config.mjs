79e4e5d60e6a281a4c3029170da98a6992314495
unknown <breezecanyve@163e.com>
2024-12-26 14:14:16 +0800

    updat config

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "/Become-a-Software-Tester/",
   title: "测试工程师笔记",
   description: "A VitePress Site",
   themeConfig: {
